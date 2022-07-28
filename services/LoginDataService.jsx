86998a14fe6a4494e657c61f1ef549d7697bccb2
camc21 <gutosjc1@gmail.com>
2022-07-28 15:11:29 -0300

    Update LoginDataService.jsx
    Alterado nome da path do login

--- services/LoginDataService.jsx
+++ services/LoginDataService.jsx
@@ -12,7 +12,7 @@ const baseUrl = `http://localhost:8081`;
 
   function logIn(email, password) {
     let data = {email, password}
-    return axios.post(baseUrl+'/auth/signin', data, {})
+    return axios.post(baseUrl+'/oauth-service/signin', data, {})
   }
 
   function logOut() {
@@ -28,7 +28,7 @@ const baseUrl = `http://localhost:8081`;
 
   function validateToken() {
     const token = localStorage.getItem('accessToken');
-    return axios.get(baseUrl+'/auth/validateToken', {
+    return axios.get(baseUrl+'/oauth-service/validateToken', {
       params: {
         token: token
       }
